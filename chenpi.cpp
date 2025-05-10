953ec8344e739d280aa2564db5bd54c9a46c5645
bazking6 <339561259@qq.com>
2025-05-10 10:18:32 +0800

    1

--- chenpi.cpp
+++ chenpi.cpp
@@ -11,20 +11,31 @@ using namespace std;
 
 //输出功能介绍，没有输入值，没有返回值
 void introduce_main() {
-
+	printf("请选择你要的操作\n");
+	printf("1.成绩输入\n");
+	printf("2.成绩导入\n");
+	printf("3.成绩导出\n");
+	printf("4.成绩查询\n");
+	printf("5.成绩分析\n");
 }
 void introduce_1() {
+	printf("请选择你要的操作\n");
+	printf("0.返回上一级\n");
 
 }
 void introduce_2() {
-
+	printf("请选择你要的操作\n");
+	printf("0.返回上一级\n");
 }
 void introduce_3() {
-
+	printf("请选择你要的操作\n");
+	printf("0.返回上一级\n");
 }
 void introduce_4() {
-
+	printf("请选择你要的操作\n");
+	printf("0.返回上一级\n");
 }
 void introduce_5() {
-
+	printf("请选择你要的操作\n");
+	printf("0.返回上一级\n");
 }
