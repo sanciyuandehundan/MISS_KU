961c501aa858a8d050b89bed69c596cc5040f59f
bazking6 <339561259@qq.com>
2025-05-20 23:05:31 +0800

    1

--- chenpi.cpp
+++ chenpi.cpp
@@ -75,7 +75,6 @@ void Introduce_delete(Log* s)
 {
     system("cls");
     Top("删除过往成绩");
-    s->Show();
     cout << COLOR_MENU;
     cout << "┌─────────────────────────────────────┐" << endl;
     cout << "│   " << COLOR_OPTION << "0.查看全部并选择指定序号成绩删除" << COLOR_MENU << "  │" << endl;
